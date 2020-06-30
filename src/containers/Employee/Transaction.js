431cc1fe8bbde05a154a374759c919824d1eabfe
Nhan1506 <lehuunhan150698@gmail.com>
2020-06-30 17:23:09 -0400

    transaction

--- src/containers/Employee/Transaction.js
+++ src/containers/Employee/Transaction.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import {
+  Button,
   Col,
   Nav,
   NavItem,
@@ -12,262 +13,278 @@ import {
   TabContent,
   TabPane,
 } from "reactstrap";
+import { connector } from "../../constants";
 
 const Transaction = () => {
   const [activeTab, setActiveTab] = useState(0);
-  return (
-    <div className="animated fadeIn">
-      <Row>
-        <Col xs="12">
-          <Nav tabs>
-            <NavItem>
-              <NavLink active={activeTab === 0} onClick={() => setActiveTab(0)}>
-                Nhận tiền
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink active={activeTab === 1} onClick={() => setActiveTab(1)}>
-                Chuyển khoản
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink active={activeTab === 2} onClick={() => setActiveTab(2)}>
-                Thanh toán nhắc nợ
-              </NavLink>
-            </NavItem>
-          </Nav>
+  const [accountNumber,setAccountNumber] = useState(3000001);
+  var rows = [];
 
-          <TabContent activeTab={activeTab}>
-            <TabPane tabId={0}>
-              <Row>
-                <Col>
-                  <Table responsive bordered>
-                    <thead>
+  const getIndex= ()=>{
+    return(
+    <div className="animated fadeIn">
+    <Row>
+      <Col xs="12">
+        <Nav tabs>
+          <NavItem>
+            <NavLink active={activeTab === 0} onClick={() => setActiveTab(0)}>
+              Nhận tiền
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active={activeTab === 1} onClick={() => setActiveTab(1)}>
+              Chuyển khoản
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink active={activeTab === 2} onClick={() => setActiveTab(2)}>
+              Thanh toán nhắc nợ
+            </NavLink>
+          </NavItem>
+        </Nav>
+
+        <TabContent activeTab={activeTab}>
+          <TabPane tabId={0}>
+            <Row>
+              <Col>
+                <Table responsive bordered>
+                  <thead>
+                    <tr>
+                      <th>Họ tên</th>
+                      <th>Tên đăng nhập</th>
+                      <th>Số tài khoản</th>
+                      <th>Số tiền</th>
+                      <th>Ngày giao dịch</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Pompeius René</td>
+                      <td>Popre</td>
+                      <td>123456789</td>
+                      <td>20.000.000</td>
+                      <td>01/01/2020</td>
+                    </tr>
+                    <tr>
+                      <td>Pompeius René</td>
+                      <td>Popre</td>
+                      <td>123456789</td>
+                      <td>20.000.000</td>
+                      <td>01/01/2020</td>
+                    </tr>
+                    <tr>
+                      <td>Pompeius René</td>
+                      <td>Popre</td>
+                      <td>123456789</td>
+                      <td>20.000.000</td>
+                      <td>01/01/2020</td>
+                    </tr>
+                    <tr>
+                      <td>Pompeius René</td>
+                      <td>Popre</td>
+                      <td>123456789</td>
+                      <td>20.000.000</td>
+                      <td>01/01/2020</td>
+                    </tr>
+                    <tr>
+                      <td>Pompeius René</td>
+                      <td>Popre</td>
+                      <td>123456789</td>
+                      <td>20.000.000</td>
+                      <td>01/01/2020</td>
+                    </tr>
+                  </tbody>
+                </Table>
+                <Pagination>
+                  <PaginationItem>
+                    <PaginationLink previous tag="button">
+                      Trước
+                    </PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem active>
+                    <PaginationLink tag="button">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem className="page-item">
+                    <PaginationLink tag="button">2</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink tag="button">3</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink next tag="button">
+                      Sau
+                    </PaginationLink>
+                  </PaginationItem>
+                </Pagination>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId={1}>
+            <Row>
+              <Col>
+                <Table responsive bordered>
+                  <thead>
+                    <tr>
+                      <th>Tên người gửi</th>
+                      <th>Tên người nhận</th>
+                      <th>Số tiền</th>
+                      <th>Ngày giao dịch</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Pompeius René</td>
+                      <td>Rens</td>
+                      <td>20.000.000</td>
+                      <td>01/01/2020</td>
+                    </tr>
+                    <tr>
+                      <td>Pompeius René</td>
+                      <td>Rens</td>
+                      <td>20.000.000</td>
+                      <td>01/01/2020</td>
+                    </tr>
+                    <tr>
+                      <td>Pompeius René</td>
+                      <td>Rens</td>
+                      <td>20.000.000</td>
+                      <td>01/01/2020</td>
+                    </tr>
+                    <tr>
+                      <td>Pompeius René</td>
+                      <td>Rens</td>
+                      <td>20.000.000</td>
+                      <td>01/01/2020</td>
+                    </tr>
+                    <tr>
+                      <td>Pompeius René</td>
+                      <td>Rens</td>
+                      <td>20.000.000</td>
+                      <td>01/01/2020</td>
+                    </tr>
+                  </tbody>
+                </Table>
+                <Pagination>
+                  <PaginationItem>
+                    <PaginationLink previous tag="button">
+                      Trước
+                    </PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem active>
+                    <PaginationLink tag="button">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem className="page-item">
+                    <PaginationLink tag="button">2</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink tag="button">3</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink next tag="button">
+                      Sau
+                    </PaginationLink>
+                  </PaginationItem>
+                </Pagination>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId={2}>
+            <Row>
+              <Col>
+                <Table responsive bordered>
+                  <thead>
+                    <tr>
+                      <th>Họ tên</th>
+                      <th>Tên đăng nhập</th>
+                      <th>Số tài khoản</th>
+                      <th>Số tiền</th>
+                      <th>Nội dung</th>
+                      <th>Ngày giao dịch</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                   {rows.map( row => {
+                      return (
                       <tr>
-                        <th>Họ tên</th>
-                        <th>Tên đăng nhập</th>
-                        <th>Số tài khoản</th>
-                        <th>Số tiền</th>
-                        <th>Ngày giao dịch</th>
+                      <td>{row.sender_account_number}</td>
+                      <td>Popre</td>
+                      <td>123456789</td>
+                      <td>20.000.000</td>
+                      <td>Trả góp</td>
+                      <td>01/01/2020</td>
                       </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Popre</td>
-                        <td>123456789</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Popre</td>
-                        <td>123456789</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Popre</td>
-                        <td>123456789</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Popre</td>
-                        <td>123456789</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Popre</td>
-                        <td>123456789</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                  <Pagination>
-                    <PaginationItem>
-                      <PaginationLink previous tag="button">
-                        Trước
-                      </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem active>
-                      <PaginationLink tag="button">1</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem className="page-item">
-                      <PaginationLink tag="button">2</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                      <PaginationLink tag="button">3</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                      <PaginationLink next tag="button">
-                        Sau
-                      </PaginationLink>
-                    </PaginationItem>
-                  </Pagination>
-                </Col>
-              </Row>
-            </TabPane>
-            <TabPane tabId={1}>
-              <Row>
-                <Col>
-                  <Table responsive bordered>
-                    <thead>
-                      <tr>
-                        <th>Tên người gửi</th>
-                        <th>Tên người nhận</th>
-                        <th>Số tiền</th>
-                        <th>Ngày giao dịch</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Rens</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Rens</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Rens</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Rens</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Rens</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                  <Pagination>
-                    <PaginationItem>
-                      <PaginationLink previous tag="button">
-                        Trước
-                      </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem active>
-                      <PaginationLink tag="button">1</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem className="page-item">
-                      <PaginationLink tag="button">2</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                      <PaginationLink tag="button">3</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                      <PaginationLink next tag="button">
-                        Sau
-                      </PaginationLink>
-                    </PaginationItem>
-                  </Pagination>
-                </Col>
-              </Row>
-            </TabPane>
-            <TabPane tabId={2}>
-              <Row>
-                <Col>
-                  <Table responsive bordered>
-                    <thead>
-                      <tr>
-                        <th>Họ tên</th>
-                        <th>Tên đăng nhập</th>
-                        <th>Số tài khoản</th>
-                        <th>Số tiền</th>
-                        <th>Nội dung</th>
-                        <th>Ngày giao dịch</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Popre</td>
-                        <td>123456789</td>
-                        <td>20.000.000</td>
-                        <td>Trả góp</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Popre</td>
-                        <td>123456789</td>
-                        <td>20.000.000</td>
-                        <td>Trả góp</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Popre</td>
-                        <td>123456789</td>
-                        <td>20.000.000</td>
-                        <td>Trả góp</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Popre</td>
-                        <td>123456789</td>
-                        <td>20.000.000</td>
-                        <td>Trả góp</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Popre</td>
-                        <td>123456789</td>
-                        <td>20.000.000</td>
-                        <td>Trả góp</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                  <Pagination>
-                    <PaginationItem>
-                      <PaginationLink previous tag="button">
-                        Trước
-                      </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem active>
-                      <PaginationLink tag="button">1</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem className="page-item">
-                      <PaginationLink tag="button">2</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                      <PaginationLink tag="button">3</PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                      <PaginationLink next tag="button">
-                        Sau
-                      </PaginationLink>
-                    </PaginationItem>
-                  </Pagination>
-                </Col>
-              </Row>
-            </TabPane>
-          </TabContent>
-        </Col>
-      </Row>
-    </div>
+                      );
+                      
+                    })}
+                    
+                  </tbody>
+                </Table>
+                
+              <Button
+                size="sm"
+                color="primary"
+                className="mx-2 px-5"
+                onClick={getTransaction}
+              >
+                <i className="fa fa-dot-circle-o"></i> Lưu
+              </Button>
+                <Pagination>
+                  <PaginationItem>
+                    <PaginationLink previous tag="button">
+                      Trước
+                    </PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem active>
+                    <PaginationLink tag="button">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem className="page-item">
+                    <PaginationLink tag="button">2</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink tag="button">3</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink next tag="button">
+                      Sau
+                    </PaginationLink>
+                  </PaginationItem>
+                </Pagination>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
+      </Col>
+    </Row>
+  </div>
+    )};
+
+  const getTransaction =  () =>{
+    const response =   connector
+    .get("/employee/get-transaction/"+accountNumber, {
+     
+    })
+    .then(
+      (response) => {
+        console.log("response", response.data);
+        rows = response.data;
+       console.log(rows);
+        console.log(rows.data.map(row =>{
+          console.log("1: "+row.sender_account_number);
+        }));
+        getIndex();
+      },
+      (error) => {
+        console.log("err123", error.response);
+        
+      }
+    );
+};
+  
+
+  return (
+   getIndex()
   );
+  
 };
 
 export default Transaction;
