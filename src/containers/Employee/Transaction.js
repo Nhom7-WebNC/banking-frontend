5df1f71e37d056eac6a1d3762a512db1db5fbb07
Nhan1506 <lehuunhan150698@gmail.com>
2020-06-30 19:04:47 -0400

    finish transaction

--- src/containers/Employee/Transaction.js
+++ src/containers/Employee/Transaction.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import {
-  Button,
+
   Col,
   Nav,
   NavItem,
@@ -13,229 +13,120 @@ import {
   TabContent,
   TabPane,
   Button,
+  Input,
+  Alert,
 } from "reactstrap";
 import { connector } from "../../constants";
 
 const Transaction = () => {
-<<<<<<< HEAD
+  const [accountNumber, setAccountNumber] = useState("123456789");
+  const [todos, setDataTable] = useState();
   const [activeTab, setActiveTab] = useState(0);
-  const [accountNumber,setAccountNumber] = useState(3000001);
-  var rows = [];
-
-  const getIndex= ()=>{
-    return(
-=======
-  const [accountNumber, setAccountNumber] = useState("230500002");
-  const [todos, setDataTable] = useState("");
-
+  const [visible, setVisible] = useState(false);
+  const [error, setError] = useState("");
+  
   const getTransaction = () => {
     console.log("account", accountNumber);
     const response = connector
       .get("/employee/get-transaction/" + accountNumber, {})
       .then(
         (response) => {
-          console.log("response", response.data.data);
+          setError("");
+          setVisible(false);
           setDataTable(response.data.data);
-          // getIndex();
+
         },
         (error) => {
           console.log("err123", error.response);
+          setError(error.response.data.msg);
+          setVisible(true);
         }
       );
   };
 
-  const [activeTab, setActiveTab] = useState(4);
-  return (
->>>>>>> 571a256a7eb634282e1b68778cb8d85282b8e76d
-    <div className="animated fadeIn">
-    <Row>
-      <Col xs="12">
-        <Nav tabs>
-          <NavItem>
-            <NavLink active={activeTab === 0} onClick={() => setActiveTab(0)}>
-              Nhận tiền
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink active={activeTab === 1} onClick={() => setActiveTab(1)}>
-              Chuyển khoản
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink active={activeTab === 2} onClick={() => setActiveTab(2)}>
-              Thanh toán nhắc nợ
-            </NavLink>
-          </NavItem>
-        </Nav>
 
-<<<<<<< HEAD
-        <TabContent activeTab={activeTab}>
-          <TabPane tabId={0}>
-            <Row>
-              <Col>
-                <Table responsive bordered>
-                  <thead>
-                    <tr>
-                      <th>Họ tên</th>
-                      <th>Tên đăng nhập</th>
-                      <th>Số tài khoản</th>
-                      <th>Số tiền</th>
-                      <th>Ngày giao dịch</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Pompeius René</td>
-                      <td>Popre</td>
-                      <td>123456789</td>
-                      <td>20.000.000</td>
-                      <td>01/01/2020</td>
-                    </tr>
-                    <tr>
-                      <td>Pompeius René</td>
-                      <td>Popre</td>
-                      <td>123456789</td>
-                      <td>20.000.000</td>
-                      <td>01/01/2020</td>
-                    </tr>
-                    <tr>
-                      <td>Pompeius René</td>
-                      <td>Popre</td>
-                      <td>123456789</td>
-                      <td>20.000.000</td>
-                      <td>01/01/2020</td>
-                    </tr>
-                    <tr>
-                      <td>Pompeius René</td>
-                      <td>Popre</td>
-                      <td>123456789</td>
-                      <td>20.000.000</td>
-                      <td>01/01/2020</td>
-                    </tr>
-                    <tr>
-                      <td>Pompeius René</td>
-                      <td>Popre</td>
-                      <td>123456789</td>
-                      <td>20.000.000</td>
-                      <td>01/01/2020</td>
-                    </tr>
-                  </tbody>
-                </Table>
-                <Pagination>
-                  <PaginationItem>
-                    <PaginationLink previous tag="button">
-                      Trước
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem active>
-                    <PaginationLink tag="button">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem className="page-item">
-                    <PaginationLink tag="button">2</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">3</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink next tag="button">
-                      Sau
-                    </PaginationLink>
-                  </PaginationItem>
-                </Pagination>
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId={1}>
-            <Row>
-              <Col>
-                <Table responsive bordered>
-                  <thead>
-                    <tr>
-                      <th>Tên người gửi</th>
-                      <th>Tên người nhận</th>
-                      <th>Số tiền</th>
-                      <th>Ngày giao dịch</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Pompeius René</td>
-                      <td>Rens</td>
-                      <td>20.000.000</td>
-                      <td>01/01/2020</td>
-                    </tr>
-                    <tr>
-                      <td>Pompeius René</td>
-                      <td>Rens</td>
-                      <td>20.000.000</td>
-                      <td>01/01/2020</td>
-                    </tr>
-                    <tr>
-                      <td>Pompeius René</td>
-                      <td>Rens</td>
-                      <td>20.000.000</td>
-                      <td>01/01/2020</td>
-                    </tr>
-                    <tr>
-                      <td>Pompeius René</td>
-                      <td>Rens</td>
-                      <td>20.000.000</td>
-                      <td>01/01/2020</td>
-                    </tr>
-                    <tr>
-                      <td>Pompeius René</td>
-                      <td>Rens</td>
-                      <td>20.000.000</td>
-                      <td>01/01/2020</td>
-                    </tr>
-                  </tbody>
-                </Table>
-                <Pagination>
-                  <PaginationItem>
-                    <PaginationLink previous tag="button">
-                      Trước
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem active>
-                    <PaginationLink tag="button">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem className="page-item">
-                    <PaginationLink tag="button">2</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">3</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink next tag="button">
-                      Sau
-                    </PaginationLink>
-                  </PaginationItem>
-                </Pagination>
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId={2}>
-            <Row>
-              <Col>
-                <Table responsive bordered>
-                  <thead>
-                    <tr>
-                      <th>Họ tên</th>
-                      <th>Tên đăng nhập</th>
-                      <th>Số tài khoản</th>
-                      <th>Số tiền</th>
-                      <th>Nội dung</th>
-                      <th>Ngày giao dịch</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                   {rows.map( row => {
-                      return (
-=======
+  return (
+    <div className="animated fadeIn">
+      <Row>
+        <Alert color="danger" isOpen={visible}>
+          {error}
+        </Alert>
+        <Col xs="12" md="9">
+          <Input
+            type = "text"
+            name="text-input"
+          placeholder="số tài khoản"
+          onChange={(e) => setAccountNumber(e.target.value)}
+          />
+        </Col>
+        <Button
+          onClick={getTransaction}
+          size="sm"
+          color="primary"
+          className="mx-2 px-5"
+        >
+          <i className="fa fa-dot-circle-o"></i> Xác nhận
+            </Button>
+        <Col xs="12">
+          <Nav tabs>
+            <NavItem>
+              <NavLink active={activeTab === 0} onClick={() => setActiveTab(0)}>
+                Nhận tiền
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active={activeTab === 1} onClick={() => setActiveTab(1)}>
+                Chuyển khoản
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active={activeTab === 2} onClick={() => setActiveTab(2)}>
+                Thanh toán nhắc nợ
+              </NavLink>
+            </NavItem>
+          </Nav>
+
           <TabContent activeTab={activeTab}>
             <TabPane tabId={0}>
               <Row>
-                <Col getTransaction></Col>
+                <Col>
+                  <Table responsive bordered>
+                    <thead>
+                      <tr>
+
+                        <th>Số tài khoản gửi</th>
+
+                        <th>Số tài khoản nhận</th>
+                        <th>Số tiền</th>
+                        <th>Lời nhắn</th>
+                        <th>Ngày giao dịch</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {todos != null ? (
+                        todos.activeTab0.map((todo) =>
+                          (
+                            <tr>
+                              <td>{todo.sender_account_number}</td>
+                              <td>{todo.receiver_account_number}</td>
+                              <td>{todo.amount}</td>
+                              <td>{todo.message}</td>
+                              <td>{todo.created_at}</td>
+                            </tr>
+                          ))) : (
+                          <tr>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                          </tr>
+                        )
+
+                      }
+                    </tbody>
+                  </Table>
+                </Col>
               </Row>
             </TabPane>
             <TabPane tabId={4}>
@@ -252,8 +143,8 @@ const Transaction = () => {
                       </tr>
                     </thead>
                     <tbody>
-                      {todos.length ? (
-                        todos.map((todo) => (
+                      {todos != null ? (
+                        todos.activeTab0.map((todo) => (
                           <tr>
                             <td>{todo.sender_account_number}</td>
                             <td>{todo.sender_bank_code}</td>
@@ -262,13 +153,13 @@ const Transaction = () => {
                           </tr>
                         ))
                       ) : (
-                        <tr>
-                          <td>-</td>
-                          <td>-</td>
-                          <td>-</td>
-                          <td>-</td>
-                        </tr>
-                      )}
+                          <tr>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                          </tr>
+                        )}
                     </tbody>
                   </Table>
                   <Pagination>
@@ -301,118 +192,36 @@ const Transaction = () => {
                   <Table responsive bordered>
                     <thead>
                       <tr>
-                        <th>Tên người gửi</th>
-                        <th>Tên người nhận</th>
+                        <th>Số tài khoản gửi</th>
+
+                        <th>Số tài khoản nhận</th>
                         <th>Số tiền</th>
+                        <th>Lời nhắn</th>
                         <th>Ngày giao dịch</th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Rens</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Rens</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
->>>>>>> 571a256a7eb634282e1b68778cb8d85282b8e76d
-                      <tr>
-                      <td>{row.sender_account_number}</td>
-                      <td>Popre</td>
-                      <td>123456789</td>
-                      <td>20.000.000</td>
-                      <td>Trả góp</td>
-                      <td>01/01/2020</td>
-                      </tr>
-<<<<<<< HEAD
-                      );
-                      
-                    })}
-                    
-                  </tbody>
-                </Table>
-                
-              <Button
-                size="sm"
-                color="primary"
-                className="mx-2 px-5"
-                onClick={getTransaction}
-              >
-                <i className="fa fa-dot-circle-o"></i> Lưu
-              </Button>
-                <Pagination>
-                  <PaginationItem>
-                    <PaginationLink previous tag="button">
-                      Trước
-                    </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem active>
-                    <PaginationLink tag="button">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem className="page-item">
-                    <PaginationLink tag="button">2</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">3</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink next tag="button">
-                      Sau
-                    </PaginationLink>
-                  </PaginationItem>
-                </Pagination>
-              </Col>
-            </Row>
-          </TabPane>
-        </TabContent>
-      </Col>
-    </Row>
-  </div>
-    )};
+                      {todos != null ? (
+                        todos.activeTab1.map((todo) =>
+                          (
+                            <tr>
+                              <td>{todo.sender_account_number}</td>
+                              <td>{todo.receiver_account_number}</td>
+                              <td>{todo.amount}</td>
+                              <td>{todo.message}</td>
+                              <td>{todo.created_at}</td>
+                            </tr>
+                          ))) : (
+                          <tr>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>-</td>
+                          </tr>
+                        )
 
-  const getTransaction =  () =>{
-    const response =   connector
-    .get("/employee/get-transaction/"+accountNumber, {
-     
-    })
-    .then(
-      (response) => {
-        console.log("response", response.data);
-        rows = response.data;
-       console.log(rows);
-        console.log(rows.data.map(row =>{
-          console.log("1: "+row.sender_account_number);
-        }));
-        getIndex();
-      },
-      (error) => {
-        console.log("err123", error.response);
-        
-      }
-    );
-};
-  
-
-  return (
-   getIndex()
-=======
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Rens</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
-                      <tr>
-                        <td>Pompeius René</td>
-                        <td>Rens</td>
-                        <td>20.000.000</td>
-                        <td>01/01/2020</td>
-                      </tr>
+                      }
                     </tbody>
                   </Table>
                   <Pagination>
@@ -496,6 +305,7 @@ const Transaction = () => {
                       </tr>
                     </tbody>
                   </Table>
+
                   <Pagination>
                     <PaginationItem>
                       <PaginationLink previous tag="button">
@@ -520,21 +330,12 @@ const Transaction = () => {
                 </Col>
               </Row>
             </TabPane>
-            <Button
-              onClick={getTransaction}
-              size="sm"
-              color="primary"
-              className="mx-2 px-5"
-            >
-              <i className="fa fa-dot-circle-o"></i> Xác nhận
-            </Button>
+
           </TabContent>
         </Col>
       </Row>
     </div>
->>>>>>> 571a256a7eb634282e1b68778cb8d85282b8e76d
   );
-  
 };
 
 export default Transaction;
